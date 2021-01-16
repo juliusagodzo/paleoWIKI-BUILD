36f547a8f4cdf6eb928445955b0646b191d7224f
lory2k01 <tecnolory01@gmail.com>
2021-01-16 10:56:40 +0100

    Add files via upload

---
+++ precache-manifest.7b01dd4cd14bf02d9e78ccceef50ffe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5311fd140da284b5200269a6089e4a6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9130f47b0d310e8a149b",
+    "url": "/static/css/main.cfe37716.chunk.css"
+  },
+  {
+    "revision": "0294576b8aaecadc5050",
+    "url": "/static/js/2.e81dfe2f.chunk.js"
+  },
+  {
+    "revision": "9130f47b0d310e8a149b",
+    "url": "/static/js/main.884e2634.chunk.js"
+  },
+  {
+    "revision": "219041030e6504a9b011",
+    "url": "/static/js/runtime~main.6c6fb276.js"
+  }
+]);
